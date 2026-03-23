7fd9dbcf8548fa7444682c67edd88d7052de8f87
teja <nistalasaivenkatateja@gmail.com>
2026-03-23 22:37:52 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,6 +16,15 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.latest.id
   instance_type = "t2.micro"
 
+  key_name = "teja-key"   
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              systemctl start nginx
+              systemctl enable nginx
+              EOF
+
   tags = {
     Name = "Teja-Server"
   }
